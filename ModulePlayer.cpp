38c33cfd8f931d84d0b785b8865e41dc28ef8b53
Joel <dynamic.cast99@gmail.com>
2017-03-22 20:29:49 +0100

    v0.1
    Added a WIP punch.

--- ModulePlayer.cpp
+++ ModulePlayer.cpp
@@ -29,7 +29,7 @@ ModulePlayer::ModulePlayer()
 	forward.PushBack({432, 131, 50, 89});
 	forward.speed = 0.1f;
 
-	//backwards animation
+	// TODO 4: Make ryu walk backwards with the correct animations
 	backward.PushBack({ 542, 131, 61, 87 });
 	backward.PushBack({ 628, 129, 59, 90 });
 	backward.PushBack({ 713, 128, 57, 90 });
@@ -37,7 +37,11 @@ ModulePlayer::ModulePlayer()
 	backward.PushBack({ 883, 128, 58, 91 });
 	backward.PushBack({ 974, 129, 57, 89 });
 	backward.speed = 0.1f;
-	// TODO 4: Make ryu walk backwards with the correct animations
+
+	//Punch animation
+	punch.PushBack({ 19, 272, 64, 91 });
+	punch.PushBack({ 108, 272, 92, 91 });
+	punch.speed = 0.2f;
 }
 
 ModulePlayer::~ModulePlayer()
@@ -64,10 +68,24 @@ update_status ModulePlayer::Update()
 		current_animation = &forward;
 		position.x += speed;
 	}
+
 	if (App->input->keyboard[SDL_SCANCODE_A] == 1)
 	{
 		current_animation = &backward;
 		position.x -= speed;
+	}
+
+	if (App->input->keyboard[SDL_SCANCODE_P] == 1)
+	{
+		if (App->input->keyboard[SDL_SCANCODE_A] == 0 && App->input->keyboard[SDL_SCANCODE_D] == 0)
+		{
+			current_animation = &punch;
+		}
+	}
+
+	if (App->input->keyboard[SDL_SCANCODE_W] == 1)
+	{
+		current_animation = &jump;
 	}
 	 
 	// Draw everything --------------------------------------
